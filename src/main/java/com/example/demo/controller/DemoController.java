3cfc06cc298b4414d641e891078ab0ed4041c833
bxhm <grow1016@163.com>
2021-04-28 11:07:37 +0800

    111

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -18,7 +18,7 @@ public class DemoController {
 	@RequestMapping(value = "test", method = { RequestMethod.POST })
 	@ApiOperation(value = "test", notes = "")
 	public Object create(HttpServletRequest request, HttpServletResponse response) {
-//		测试
+//		测试111
 		JSONObject result = new JSONObject();
 		result.put("msg", "success!!!");
 		return result;
